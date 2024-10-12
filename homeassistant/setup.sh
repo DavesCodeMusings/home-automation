0594ee21e9608f8240a226f87a32bf93d5c74071
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-10-12 13:51:48 -0500

    Update setup.sh

--- homeassistant/setup.sh
+++ homeassistant/setup.sh
@@ -5,10 +5,12 @@
 BASE_DIR=$(pwd)
 ESPHOME_DIR=$BASE_DIR/esphome
 HASS_DIR=$BASE_DIR/hass
+NGINX_DIR=$BASE_DIR/nginx
 
 echo "Creating directory structure."
 install -d -o0 -g0 -m755 $ESPHOME_DIR/config
 install -d -o0 -g0 -m755 $HASS_DIR/config
+install -d -o0 -g0 -m755 $NGINX_DIR/conf.d
 
 echo "Creating compose.yml."
 cat <<EOF >$BASE_DIR/compose.yml
@@ -33,6 +35,30 @@ services:
       - $HASS_DIR/config:/config
     network_mode: host
     restart: unless-stopped
+
+  nginx:
+    container_name: nginx_hass
+    hostname: nginx
+    image: nginx
+    ports:
+      - 8080:80
+      - 8443:443
+    volumes:
+      - /etc/ssl:/etc/ssl:ro
+      - $NGINX_DIR/conf.d:/etc/nginx/conf.d
+      - /srv/www:/usr/share/nginx/html:ro
+    networks:
+      reverse_proxy:
+        ipv4_address: 172.16.0.2
+    restart: unless-stopped
+
+networks:
+  reverse_proxy:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 172.16.0.0/24
+          gateway: 172.16.0.1
 EOF
 
 echo
